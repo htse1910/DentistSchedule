e4c6e59c2cce01e8c8d105e6830fe3344e758489
thanghung18062003 <hungpltse172380@fpt.edu.vn>
2024-06-29 19:58:46 +0700

    crud-c

--- services/CRUDService.js
+++ services/CRUDService.js
@@ -71,6 +71,28 @@ let getUserInfoById = (userId) => {
 }
 
 let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: data.id }
+            })
+            if (user) {
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+
+                await user.save();
+                let allUsers = await db.User.findAll();
+                resolve(allUsers);
+            } else {
+                resolve();
+            }
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    );
 }
 
 let deleteUserById = (userId) => {
